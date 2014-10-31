65c7c1c24a48df70c0bc655f6e8ee57aa6983f80
Michael Truog <mjtruog@gmail.com>
2014-10-30 17:25:16 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule CloudIServiceDbCassandraCql do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_db_cassandra_cql,
+     version: "1.4.0-rc.4",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:erlcql,
+      [git: "https://github.com/rpt/erlcql.git",
+       branch: "develop"]},
+     {:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Cassandra CQL Service"
+  end
+
+  defp package do
+    [files: ~w(src doc test rebar.config README.markdown),
+     contributors: ["Irina Guberman", "Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_db_cassandra_cql"}]
+   end
+end
