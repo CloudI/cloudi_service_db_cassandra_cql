ec96c0cfc8008407ba97d33b7a4b20274bc98b41
Michael Truog <mjtruog@gmail.com>
2015-04-12 21:07:16 -0700

    Version 1.5.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceDbCassandraCql do
 
   def project do
     [app: :cloudi_service_db_cassandra_cql,
-     version: "1.4.0",
+     version: "1.5.0",
      language: :erlang,
      description: description,
      package: package,
@@ -14,7 +14,7 @@ defmodule CloudIServiceDbCassandraCql do
     [{:erlcql,
       [git: "https://github.com/rpt/erlcql.git",
        branch: "develop"]},
-     {:cloudi_core, "~> 1.4.0"}]
+     {:cloudi_core, "~> 1.5.0"}]
   end
 
   defp description do
